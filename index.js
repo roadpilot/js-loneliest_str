5d9e639f67c23f028018cdc0ad9633ea91c1b8d5
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-08-27 15:30:08 +0000

    solution

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 function loneliest(str) {
-
+  console.log(str.split(/[a-z]/))
+//   console.log(str.includes(' '))
+  if (str.includes(' ')){
+//     str = str.trim()
+    alpha = {}
+    let spacecount = 0
+    const arr = str.trim().split('')
+    for (let i=0; i<arr.length; i++){
+      if (arr[i] !== ' ') {
+        console.log(arr[i])
+        spacecount = 0
+      }
+        spacecount++
+    }
+    console.log(spacecount)
+  } else {
+    return str.split('')
+  }
+//   return Array(str)
 }
